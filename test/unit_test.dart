90052981d00ee2b998924d474a4acb8fde93d018
Memory Wang <memory5776@gmail.com>
2023-09-18 17:49:16 +0800

    add unit test, widget test.

---
+++ test/unit_test.dart
@@ -0,0 +1,32 @@
+import 'package:mockito/mockito.dart';
+import 'package:practice_loading_images/models/image_gallery_model.dart';
+import 'package:practice_loading_images/services/APIResponse.dart';
+import 'package:practice_loading_images/services/image_repository.dart';
+import 'package:test/test.dart';
+
+class MockImageRepository extends Mock implements ImageRepository {}
+
+Future<void> main() async {
+  group('getPhotos', () {
+    test('return APIResponse is successful',
+            () async {
+          var result = await ImageRepository().getPhotos();
+          expect(result, isA<APIResponse>());
+    });
+  });
+
+  group('getPhotosMock', () {
+    test('return APIResponse is successful', () async {
+      final repository = MockImageRepository();
+
+      when(repository.getPhotos()).thenAnswer((_) async =>
+          APIResponse(statusCode: 200, value: [
+            ImageGalleryModel(1, 1, 'title', 'url', 'thumbnailUrl').toJson()
+          ]));
+
+      expect(await repository.getPhotos(), isA<APIResponse>());
+
+    });
+
+  });
+}
